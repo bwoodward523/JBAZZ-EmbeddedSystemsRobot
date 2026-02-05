392c34a9db6aa6c8e9548b9e68e2b48631b36cfd
bwoodward523 <bwoodward@ycp.edu>
2026-02-05 07:21:52 -0500

    wip

--- ServerClient/whisperserver.py
+++ ServerClient/whisperserver.py
@@ -5,6 +5,13 @@ from faster_whisper import WhisperModel
 import soundfile as sf
 import io
 
+import requests
+import json 
+
+from ollama_funcs import send_prompt_local
+
+from dia.model import Dia
+
 
 HOST = "0.0.0.0"
 PORT = 5555
@@ -53,36 +60,46 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 
             audio_chunks.append(chunk)
 
+        #Turn the incoming chunk of bytes into a buffer. Then send this buffer to whisper model.
+        buffer = io.BytesIO(chunk)
+        #Name the buffer as a file so whisper accepts it
+        buffer.name = "audio.mp3"
+
         print("Audio received, transcribing...")
 
-        audio_bytes = b"".join(audio_chunks)
+        #Sending the buffer to whisper.
+        segments, info = model.transcribe(buffer, language="en")
 
-    
-        with io.BytesIO(audio_bytes) as f:
-            data, samplerate = sf.read(f, dtype="float32")  # returns float32 PCM in range [-1, 1]
-
-        # data shape: (num_samples,) for mono, (num_samples, 2) for stereo
-        # Whisper expects mono, so convert if needed
-        if len(data.shape) > 1:
-            data = data.mean(axis=1)  # stereo → mono
-
-        print(f"Audio len {len(audio_bytes)}")
-        # Convert bytes → PCM float32
-        pcm = (
-            np.frombuffer(audio_bytes, dtype=np.int16)
-            .astype(np.float32) / 32768.0
-        )
-
-        segments, info = model.transcribe(data, language="en")
-
+        #Get the response and turn it into a string
         transcript = " ".join(seg.text.strip() for seg in segments)
-        transcript_bytes = transcript.encode("utf-8")
+        #Send the string back to the client
+        #TODO: Update this to send the string instead to Ollama. 
 
-        for segment in segments:
-            print("there are segments")
-            print("[%.2fs -> %.2fs] %s" % (segment.start, segment.end, segment.text))
+        ollama_resp = send_prompt_local(transcript)
+
+
+        transcript_bytes = ollama_resp.encode("utf-8")
+
+        tts_model = Dia.from_pretrained("nari-labs/Dia-1.6B-0626")
+
+
         # Send transcript back
         conn.sendall(struct.pack("!I", len(transcript_bytes)))
         conn.sendall(transcript_bytes)
 
         print("Transcript sent")
+
+
+        text = f"[S1] Hello World"
+
+        output = tts_model.generate(
+            text,
+            use_torch_compile=False,
+            verbose=True,
+            cfg_scale=3.0,
+            temperature=1.8,
+            top_p=0.90,
+            cfg_filter_top_k=50,
+        )
+
+        tts_model.save_audio("simple.mp3", output)
